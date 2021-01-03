18a63c3b0c9f5e9ad530ba459f833810a03c788f
LBJ_nobe <1229413925@qq.com>
2021-01-03 23:55:07 +0800

    用户个人信息界面初步完成 剩余样式，账号绑定，和修改密码待coding

---
+++ src/pages/basicinfo/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+// 引入css样式
+import Style from "./ProfileInfo.module.css"
+
+// 引入本地localStorage
+import methods from "../../../utils/storageUtils"
+
+//引入查询用户请求
+import { QueryUserByUsername} from "../../../network/index"
+
+// 引入头像
+import userHeader from "../../../assets/image/user-header.jpg"
+
+export default class ProfileInfo extends Component {
+    state={
+        usersInfo:[]
+    }
+
+    // 请求用户信息
+    GetUserInfo=async()=>{
+        const username = methods.getUser()
+        const result = await QueryUserByUsername(username);
+        this.setState({
+            usersInfo:result.data
+        })
+    }
+
+    // 生命周期内请求用户信息
+    componentDidMount(){
+        this.GetUserInfo()
+    }
+
+    render() {
+        // console.log(this.state.usersInfo);
+        const {usersInfo}=this.state
+        console.log(usersInfo);
+        if(usersInfo.length!==0){
+            var {email,username,realname,phoneNumber,depart,id}=usersInfo[0]
+            console.log(email);
+        }else{
+            var email,username,realname,phoneNumber,depart,id=''
+        }
+        // var email='123'
+        return (
+            <div className={Style.profileinfo}>
+                <img src={userHeader} alt=""/>
+                <div className={Style.username}>{realname}</div>
+                <div className={Style.userinfo}>
+                    <div><span>电子信箱:</span>{email}</div>
+                    <div><span>用户名:</span>{username}</div>
+                    <div><span>联系电话:</span>{phoneNumber}</div>
+                    <div><span>所属部门:</span>{depart}</div>
+                    <div><span>推荐理由:</span></div>
+                    <div><span>员工编号:</span>{id}</div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/basicinfo/index.jsx
+++ src/pages/basicinfo/index.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
+// 从antd中引入tabs
+import { Tabs } from "antd";
+
+// 引入css样式
+import "./basicinfo.less"
+
+// 引入子组件
+import ProfileInfo from "./ProfileInfo/ProfileInfo"
+
+const { TabPane } = Tabs;
+
 export default class BasicInfo extends Component {
+
+
+    // 定义tabs onChange函数
+    callback = (key) => {
+        console.log(key);
+    }
+
     render() {
         return (
-            <div>
-                基本信息
+            <div className="myProfile123">
+                <div className="tab">
+                    <Tabs defaultActiveKey="1" onChange={this.callback}
+                    centered='true'>
+                    <TabPane tab="个人信息" key="1"
+                    style={{position:'relative'}}>
+                            <ProfileInfo />
+                    </TabPane>
+                        <TabPane tab="账号绑定" key="2">
+                            账号绑定
+                    </TabPane>
+                        <TabPane tab="修改密码" key="3">
+                            <div className="passwd">原密码: <input type="password" placeholder="请输入原密码"/></div>
+                            <div className="passwd">新密码: <input type="password" placeholder="请输入新密码"/></div> 
+                            <div className="passwd">确认密码: <input type="password" placeholder="请输入确认密码"/></div> 
+                            <button>保存</button>
+                    </TabPane>
+                    </Tabs>
+                </div>
             </div>
         )
     }
