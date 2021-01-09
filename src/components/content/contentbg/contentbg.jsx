c012dd3e4c83792722ade1558060467343837fb4
LBJ_nobe <1229413925@qq.com>
2021-01-09 20:39:24 +0800

    搜索功能的实现和外框样式BUG的解决

--- src/components/content/contentbg/contentbg.jsx
+++ src/components/content/contentbg/contentbg.jsx
@@ -3,17 +3,28 @@ import React, { Component } from 'react'
 // 引入图片
 import contentbg from "../images/adminbg.jpg"
 
+//引入发布与订阅方式
+import PubSub from "pubsub-js" 
+
 // 引入css样式
 import "./contentbg.less"
 
 export default class ContentBg extends Component {
+
+    // 点击搜索后获取当前内容，并返回后台查询
+    handleClickSearchJob=()=>{
+        // console.log(this.searchJob.value);
+        PubSub.publish("SearchJob",this.searchJob.value)
+    }
+
+
     render() {
         return (
             <div className="bg">
                 <img src={contentbg} alt=""/>
                 <div className="searchban">
-                <input className="search" type="text" placeholder="搜索职位" />
-                <button>搜索</button>  
+                <input style={{outline:'none'}} className="search" type="text" placeholder="搜索职位" ref={input=>this.searchJob=input}/>
+                <button style={{outline:'none'}} onClick={this.handleClickSearchJob}>搜索</button>  
                 </div>
                 
                              
